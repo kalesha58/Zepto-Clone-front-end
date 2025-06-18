6a4d3eab7ac392bc8fb275d68b6c2ad024a5c9ed
kalesha58 <bablu05786bablu@gmail.com>
2025-06-18 22:38:10 +0530

    properly update the androidxml file

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Text, TextInput} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+import {
+     configureReanimatedLogger, ReanimatedLogLevel
+} from 'react-native-reanimated'
+Text.defaulutProps={}
+TextInput.defaulutProps={}
 
 AppRegistry.registerComponent(appName, () => App);
